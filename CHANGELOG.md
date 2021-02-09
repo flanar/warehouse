5e2b3140e1f0cc74eee7592e7741a1b0c0c144da
flanar <piotr.suchorab@giss.pl>
2021-02-09 13:16:25 +0100

    Add and configure tailwindcss, implement switching beetwen light and dark mode in Navigation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,8 @@
 ## 0.0.1
 
 - Initial boiler plate
+
+## 0.0.2
+
+- Add and configure tailwindcss
+- Implement switching beetwen light and dark mode in Navigation
--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,16 +1,38 @@
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
 
-const Nav = () => (
-    <nav>
-        <ul>
-            <li><Link href="/"><a>Home</a></Link></li>
-            <li><Link href="/costumes"><a>Costumes</a></Link></li>
-            <li><Link href="/regions"><a>Regions</a></Link></li>
-            <li><Link href="/types"><a>Types</a></Link></li>
-            <li><Link href="/members"><a>Members</a></Link></li>
-            <li><Link href="/concerts"><a>Concerts</a></Link></li>
-        </ul>
-    </nav>
-)
+import Sun from './_assets/sun.svg'
+import Moon from './_assets/moon.svg'
+
+const Nav = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false)
+
+    useEffect(() => {
+        if(localStorage.getItem('darkMode') === 'true') {
+            document.body.classList.toggle('dark')
+            setIsDarkMode(true)
+        }
+    }, [])
+
+    const changeMode = () => {
+        localStorage.setItem('darkMode', (!isDarkMode).toString())
+        setIsDarkMode(!isDarkMode)
+        document.body.classList.toggle('dark')
+    }
+
+    return (
+        <nav>
+            <ul>
+                <li className='text-red-300 dark:text-blue-800'><Link href="/"><a>Home</a></Link></li>
+                <li><Link href="/costumes"><a>Costumes</a></Link></li>
+                <li><Link href="/regions"><a>Regions</a></Link></li>
+                <li><Link href="/types"><a>Types</a></Link></li>
+                <li><Link href="/members"><a>Members</a></Link></li>
+                <li><Link href="/concerts"><a>Concerts</a></Link></li>
+                <li className='cursor-pointer' onClick={ changeMode } >{isDarkMode ? <Moon width='20px' /> : <Sun width='20px' />}</li>
+            </ul>
+        </nav>
+    )
+}
 
 export default Nav
